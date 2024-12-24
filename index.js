3502c4fa0fff97e4753a4a66805a2ea9ca7d1b11
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2024-12-24 21:36:35 +0530

    first commit5

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(`${process.env.MONGO_URI}/events`, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
